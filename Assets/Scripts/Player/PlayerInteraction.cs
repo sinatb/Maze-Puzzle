20a3d0606994397885967a6c4fd6ae858d11c047
sinatb <sinatb.dev@gmail.com>
2024-10-23 12:07:15 +0330

    player-004 : Added player interaction script

---
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] private Transform _interactionPoint;
+    [SerializeField] private float _interactionPointRadius = 1.0f;
+    [SerializeField] private LayerMask _interactionMask;
+
+    private readonly Collider[] _colldiers = new Collider[3];
+
+    private void Update()
+    {
+        Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colldiers, _interactionMask);   
+    }
+}
