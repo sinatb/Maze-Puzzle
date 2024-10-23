eed554329a23987fbf17609aa22d1f5330772f6f
sinatb <sinatb.dev@gmail.com>
2024-10-23 12:49:29 +0330

    player-007 : door animations and scripts completed

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
 {
-    [SerializeField] private string _InteractionName = "Open Door";
-    public string InteractionName => _InteractionName;
-
+    [SerializeField] private string _interactionName = "Open Door";
+    public string InteractionName => _interactionName;
+    private Animator _animator;
+    private bool _isOpen = false;
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();  
+    }
+    private void ToggleDoor()
+    {
+       if (_isOpen) {
+            _animator.Play("door_close", 0, 0.0f);
+            _isOpen = false;
+       } else {
+            _animator.Play("door_open", 0, 0.0f);
+            _isOpen = true;
+       }
+    }
     public void Interact(PlayerInteraction pi)
     {
-        
-        
+        ToggleDoor();
     }
 }
