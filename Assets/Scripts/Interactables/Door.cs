445afa17a85cb6440d480d6e426d859ed219c73b
sinatb <sinatb.dev@gmail.com>
2024-10-24 16:30:42 +0330

    player-011: desk and audio added

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class Door : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _interactionName = "Open Door";
+    public AudioClip doorOpen;
+    public AudioClip doorClose;
     public string InteractionName => _interactionName;
     private Animator _animator;
     private bool _isOpen = false;
+    private AudioSource _audioSource;
     private void Start()
     {
         _animator = GetComponent<Animator>();  
+        _audioSource = GetComponent<AudioSource>();
     }
     private void ToggleDoor()
     {
        if (_isOpen) {
             _animator.Play("door_close", 0, 0.0f);
+            _audioSource.PlayOneShot(doorClose);
             _isOpen = false;
        } else {
             _animator.Play("door_open", 0, 0.0f);
+            _audioSource.PlayOneShot(doorOpen);
             _isOpen = true;
        }
     }
