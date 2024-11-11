9b226e01badc2b42517200052006f64a6d6e0ba6
sinatb <sinatb.dev@gmail.com>
2024-11-11 19:18:51 +0330

    fix : player long distance interaction bug fix

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -7,7 +7,8 @@ public class PlayerInteraction : MonoBehaviour
     [SerializeField] private Transform _interactionPoint;
     [SerializeField] private float _interactionPointRadius = 1.0f;
     [SerializeField] private LayerMask _interactionMask;
-
+    private bool _isWatchingCollider = false;
+    private RaycastHit _hit;
     private readonly Collider[] _colldiers = new Collider[3];
 
     private void Update()
@@ -15,9 +16,23 @@ public class PlayerInteraction : MonoBehaviour
         Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colldiers, _interactionMask);   
         if (_colldiers[0] != null)
         {
-            if (Input.GetKeyUp(KeyCode.E))
+            if (Input.GetKeyUp(KeyCode.E) && _isWatchingCollider)
             {
                 _colldiers[0].GetComponent<IInteractable>().Interact(this);
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out _hit, _interactionMask))
+        {
+            if (_colldiers[0] != null && _hit.transform.gameObject == _colldiers[0].gameObject)
+            {
+                _isWatchingCollider = true;
+            }
+            else
+            {
+                _isWatchingCollider = false;
             }
         }
     }
